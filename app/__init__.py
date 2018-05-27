d134015db281a150ac37a9bf8cfd281678ae68ad
GingerTea <drak0sha@tut.by>
2018-05-27 21:51:03 +0300

    localization ru

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,8 @@ import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
-from flask import Flask
+from flask import Flask, request
+from flask_babel import Babel, lazy_gettext as _l
 from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -19,12 +20,22 @@ migrate = Migrate(app, db)
 
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 
 mail = Mail(app)
 
 bootstrap = Bootstrap(app)
 
 moment = Moment(app)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    print(request.accept_languages)
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
