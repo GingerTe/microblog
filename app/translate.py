370037fd9132ba7ef8b429eba561bae40d913ce0
GingerTea <drak0sha@tut.by>
2018-05-28 00:26:44 +0300

    auth an main blueprints

--- app/translate.py
+++ app/translate.py
@@ -1,17 +1,17 @@
 import json
 import requests
+from flask import current_app
 from flask_babel import _
-from app import app
 
 
 def translate(text, source_language, dest_language):
-    if 'YA_TRANSLATOR_KEY' not in app.config or \
-            not app.config['YA_TRANSLATOR_KEY']:
+    if 'YA_TRANSLATOR_KEY' not in current_app.config or \
+            not current_app.config['YA_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
 
     root_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     r = requests.get(root_url,
-                     params=dict(key=app.config['YA_TRANSLATOR_KEY'],
+                     params=dict(key=current_app.config['YA_TRANSLATOR_KEY'],
                                  text=text,
                                  lang="%s-%s" % (source_language, dest_language))
                      )
