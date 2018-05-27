0e7338d21b9248f9f3f2b2a025e9d0c8b6fd0026
GingerTea <drak0sha@tut.by>
2018-05-27 17:20:36 +0300

    bootstrap

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,13 @@ import logging
 import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
+from config import Config
 from flask import Flask
 from flask_login import LoginManager
 from flask_mail import Mail
-
-from config import Config
-from flask_sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
 from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,6 +19,8 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 mail = Mail(app)
+
+bootstrap = Bootstrap(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
