ff5b430df057c86b29a5eaea85441f9ec9400fd3
GingerTea <drak0sha@tut.by>
2018-05-29 23:17:42 +0300

    ajax popover

--- app/main/routes.py
+++ app/main/routes.py
@@ -148,6 +148,13 @@ def search():
                            next_url=next_url, prev_url=prev_url)
 
 
+@bp.route('/user/<username>/popup')
+@login_required
+def user_popup(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    return render_template('user_popup.html', user=user)
+
+
 @bp.before_app_request
 def before_request():
     if current_user.is_authenticated:
